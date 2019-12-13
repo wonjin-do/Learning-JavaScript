dc05e16d2f56bce419213c41f2cab77d54b6868c
wonjin-do <54175950+wonjin-do@users.noreply.github.com>
2019-12-14 01:24:45 +0900

    resolve와 reject의미 update

--- 코드종YouTube정리/코드종callback지옥 promis로 탈출하기.js
+++ 코드종YouTube정리/코드종callback지옥 promis로 탈출하기.js
@@ -9,6 +9,8 @@ function delay(sec, callback){
 // });
 
 function delayP(sec){
+    //resolve: 할 일을 다했을 때 호출
+    //reject: 할 일을 하던중 예외가 날 때 호출
     return new Promise((resolve, reject) => {
         setTimeout(()=>{
             resolve(new Date().toISOString());
@@ -27,5 +29,3 @@ delayP(1).then((res) => {
  }).then((res) => {
     console.log(res);
  });
-
-
